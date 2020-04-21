50d44ea196eeacdf228a2f3861cf2d961954eabe
adnrbp <adnrbp@users.noreply.github.com>
2020-04-21 13:27:44 -0500

    [init] move config

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'music_assistant.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'music_assistant.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
