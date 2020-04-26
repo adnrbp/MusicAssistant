1d806088d86109cd9181f8188bbb6404eac653ef
adnrbp <adnrbp@users.noreply.github.com>
2020-04-26 01:13:11 -0500

    [model] user: save and get by sender_id

--- music_assistant/users/models/users.py
+++ music_assistant/users/models/users.py
@@ -13,9 +13,16 @@ class User(models.Model):
         )
 
     @classmethod
-    def register_new_user(cls, remote_id, first_name, last_name):
+    def register_new_user(cls, sender_id, user_name, last_name):
         return cls.objects.get_or_create(
                     remote_id=sender_id,
                     first_name=user_name,
                     last_name=last_name
                 )
+    @classmethod
+    def get_user_by_sender_id(cls,sender_id):
+        try:
+            user = cls.objects.get(remote_id=sender_id)
+        except User.DoesNotExist:
+            user = None
+        return user
